ee50ef2784f53ead47c0063010e0e0ede4206480
Teddy Okal <96052598+OKUKUOKAL@users.noreply.github.com>
2023-05-26 05:39:45 +0300

    Delete Array.py

--- Array.py
+++
@@ -1,8 +0,0 @@
-# Define  array
-arr = [13, 17, 25, 21, 43, 62, 86, 95]
-
-# sum of elements in the array
-sum_of_elements = sum(arr)
-
-# Print the o/p
-print("T sum of elements in the array is:", sum_of_elements)
